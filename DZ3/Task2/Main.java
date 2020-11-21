671fc1357340b065d4c76f4956667d43dc6fa7b5
Murat Dibirov <dshoma853@gmail.com>
2020-11-21 20:02:02 +0300

    добавил условие и чуть изменил решение

--- DZ3/Task2/Main.java
+++ DZ3/Task2/Main.java
@@ -2,6 +2,9 @@ package DZ3.Task2;
 import java.util.LinkedList;
 import java.util.ArrayList;
 
+//2) Напишите метод, который добавляет 1.000.000 элементов в ArrayList и LinkedList.
+//Наугад берет по 10.000 элементов с каждой коллекции.Замерьте время, кто быстрее.
+
 public class Main {
     public static void main(String[] args) {
             compare2Lists();
@@ -26,7 +29,7 @@ public class Main {
         System.out.println("Вермя ArrayList: " + arrayTime);
 
         startTime = System.currentTimeMillis();
-        for(int i=0; i<N;i++){
+        for(int i=0; i<M;i++){
             linkedList.get((int) Math.random()*(M-1));
         }
         linkedTime = System.currentTimeMillis() - startTime;
