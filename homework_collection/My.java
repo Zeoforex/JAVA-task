488eb72b4529afe1692fd24cc3cd3873b2995e71
Murat Dibirov <dshoma853@gmail.com>
2020-11-22 17:33:03 +0300

    добавил условие

--- homework_collection/My.java
+++ homework_collection/My.java
@@ -4,6 +4,11 @@ import java.util.Iterator;
 import java.util.Stack;
 import java.util.NoSuchElementException;
 
-public class My implements Iterator<String>{
+//Дан итератор.
+//Метод next() возвращает либо String, либо итератор такой же структуры
+//(то есть который опять возвращает или String, или такой же итератор).
+//Напишите поверх этого итератора другой, уже «плоский»
 
+public class My implements Iterator<String>{
+    
 }
