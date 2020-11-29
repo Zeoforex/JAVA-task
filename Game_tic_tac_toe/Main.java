0b78b0f6d8f2066c41a3acc2d12c6f5d18035430
Murat Dibirov <dshoma853@gmail.com>
2020-11-29 14:35:57 +0300

    начало игры и создание файла главного

--- Game_tic_tac_toe/Main.java
+++ Game_tic_tac_toe/Main.java
@@ -1,5 +1,12 @@
 package Game_tic_tac_toe;
 
-public class Main {
 
+// буду делать игру крестики нолики
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Отсюда можно будет начать играть запустив main");
+        TicTacToe game = new TicTacToe();
+        game.game();
+    }
 }
